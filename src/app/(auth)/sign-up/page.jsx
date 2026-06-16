b40e9aa2e8928d0cf168a28aa175e7af885434c3
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-06-16 22:37:45 +0600

    first commit

---
+++ src/app/(auth)/sign-up/page.jsx
@@ -0,0 +1,9 @@
+const SignUpPage = () => {
+    return (
+        <div className="bg-blue-500 h-screen w-screen">
+            <h1>SignUp Page</h1>
+        </div>
+    );
+};
+
+export default SignUpPage;
---
+++ src/app/(auth)/sing-in/page.jsx
@@ -0,0 +1,9 @@
+const SignInPage = () => {
+    return (
+        <div className="bg-red-500 h-screen w-screen">
+            <h1>SignIn Page</h1>
+        </div>
+    );
+};
+
+export default SignInPage;

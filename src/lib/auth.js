592d8a06c07f4331e9f6abb95197036c5e19d5d4
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-06-17 17:21:50 +0600

    feat: initialize Better Auth configuration with MongoDB adapter and JWT support

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins/jwt";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("Bazaro");
@@ -23,4 +24,12 @@ export const auth = betterAuth({
       },
     },
   },
+  session: {
+    cookieCache: {
+      enabled: true,
+      strategy: "jwt",
+      maxAge: 60 * 24 * 30,
+    },
+  },
+  plugins: [jwt()],
 });
